0197a21b636b39bf8217fcba93c70ac7c7feac03
Daniel <danielgongar8@gmail.com>
2025-11-28 20:10:32 +0100

    subida del backend

---
+++ montero-daniel/src/main/java/com/gogaming/montero_daniel/MonteroDanielApplication.java
@@ -0,0 +1,13 @@
+package com.gogaming.montero_daniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonteroDanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonteroDanielApplication.class, args);
+	}
+
+}
---
+++ montero-daniel/src/test/java/com/gogaming/montero_daniel/MonteroDanielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogaming.montero_daniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonteroDanielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
